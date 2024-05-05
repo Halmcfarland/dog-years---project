5d003a979219c235f776c5a70a7f943fb0b72d2f
Hal <168375917+Halmcfarland@users.noreply.github.com>
2024-05-05 13:22:19 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const myAge = 35;
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5; 
+let laterYears = myAge - 2;
+laterYears = laterYears * 4; 
+let myAgeInDogYears = earlyYears + laterYears; 
+const myName = 'Hal'.toLowerCase(); 
+
+console.log(myAgeInDogYears); 
+console.log(myName); 
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
